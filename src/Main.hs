dcebb81d67e20c8dbe5b4960fd2e5359e56d47e5
PSI <ledyba@users.osdn.me>
2015-07-12 00:24:26 +0900

    fic

--- src/Main.hs
+++ src/Main.hs
@@ -16,11 +16,16 @@ helloWorld = "+++++ +++[- >++++ ++++< ]>+++ +++++ .<+++ ++[-> +++++ <]>++"++
 easyloop :: String
 easyloop = "++[-]"
 
+intape :: [Int]
+intape = [0]
+
+src :: String
+src = helloWorld
+
 create :: IO()
 create = do
   putStrLn "Parsing..."
-  let Right ast = P.parse helloWorld
-  let intape = [0]
+  let Right ast = P.parse src
   putStrLn "To CNF..."
   let cnf = C.toCNF $ C.removeNot $ S.gen ast intape
   putStrLn $ show (length cnf) ++ " clauses, " ++ show (foldl (\t a -> t + length (a)) 0 cnf) ++ " literals"
@@ -31,34 +36,36 @@ create = do
   writeFile "sat.txt" (C.toDMACS isat dict)
   putStrLn "All done, have fun."
 
+check :: IO ()
+check = do
+  let Right ast = P.parse src
+  let ids = E.run ast intape S.tapeLength S.timeLength
+  preds <- fmap (read :: String -> [(Int, Component)]) (readFile "pred.txt")
+  ansStr <- readFile "ans.txt"
+  let ans = R.fromDMACS preds ansStr
+  let val = D.valuation (fmap fst ans) intape ids
+  print $ "Exec " ++ show (length ids) ++ " Steps"
+  let pairs = (\((p1,a),(p2,v)) -> if p1 == p2 then (p1,a,v) else error "???" ) <$> zip ans val
+  let notmatched = filter (\(_,a,v) -> a /= v) pairs
+  print $ "Do not match: " ++ show (length notmatched) ++ " items"
+  print notmatched
+
+test :: IO ()
+test = do
+  let Right ast = P.parse src
+  let ids = E.run ast intape S.tapeLength S.timeLength
+  let sat = S.gen ast intape
+  let r = D.eval  sat intape ids
+  print $ show src
+  putStrLn $ L.intercalate "\n" $ fmap (\(idx, it) -> show idx ++ ": " ++ show it) (zip ([0.. ] :: [Int]) ids)
+  -- print $ show sat
+  print $ show r
+
 main :: IO ()
 main = do
   argv <- getArgs
   case argv of
     ("create":_) -> create
-    ("check":_) -> do
-      preds <- fmap (read :: String -> [(Int, Component)]) (readFile "pred.txt")
-      ansStr <- readFile "ans.txt"
-      let ans = R.fromDMACS preds ansStr
-      let val = D.valuation (fmap fst ans) intape ids
-      print $ "Exec " ++ show (length ids) ++ " Steps"
-      let pairs = (\((p1,a),(p2,v)) -> if p1 == p2 then (p1,a,v) else error "???" ) <$> zip ans val
-      let notmatched = filter (\(_,a,v) -> a /= v) pairs
-      print $ "Do not match: " ++ show (length notmatched) ++ " items"
-      print notmatched
-    ("test":_) -> do
-      print $ show src
-      putStrLn $ L.intercalate "\n" $ fmap (\(idx, it) -> show idx ++ ": " ++ show it) (zip ([0.. ] :: [Int]) ids)
-      -- print $ show sat
-      print $ show r
+    ("check":_) -> check
+    ("test":_) -> test
     _ -> print "(>_<)"
-  where
-    src = helloWorld
-    Right ast = P.parse src
-    intape = [0]
-    ids = E.run ast intape S.tapeLength S.timeLength
-    sat = S.gen ast intape
-    r = D.eval  sat intape ids
-    rnot = C.removeNot sat
-    cnf = C.toCNF rnot
-    (isat, dict) = C.alias cnf
